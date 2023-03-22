b84c684d18070e3f87604af3bb3ab459dd82d9cd
Mats100 <muneeb75@icloud.com>
2023-03-22 18:28:15 +0500

    create directory

--- file_ops/main.py
+++ file_ops/main.py
@@ -1,6 +1,6 @@
 import os
 
-os.makedirs('myfiles', exist_ok=True)
+os.makedirs('../myfiles', exist_ok=True)
 for i in range(5):
     file_name = f'myfiles/{i}.txt'
     with open(file_name, "w+") as f:
