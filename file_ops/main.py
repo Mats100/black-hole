55f8350276e059b4755fa627794d1d2f810906a2
Mats100 <muneeb75@icloud.com>
2023-03-22 18:53:31 +0500

    code changed

--- file_ops/main.py
+++ file_ops/main.py
@@ -1,7 +1,7 @@
 import os
 
-os.makedirs('../myfiles', exist_ok=True)
+os.makedirs('myfiles', exist_ok=True)
 for i in range(5):
     file_name = f'myfiles/{i}.txt'
-    with open(file_name, "w+") as f:
+    with open(file_name, "w") as f:
         f.write(str(i) + "\n")
