cf5636022e981f48d1c72c54c3772a81043e60ca
Mats100 <muneeb75@icloud.com>
2023-03-27 17:56:28 +0500

    more code added

--- input_ops/user_choices.py
+++ input_ops/user_choices.py
@@ -1,8 +1,18 @@
-choices = int(input("Enter number of choices:"))
 data = []
+choices = int(input("Enter number of choices:"))
 for i in range(choices):
     name_input = str(input(f'Enter the name {i + 1}:'))
     data.append(name_input)
-    print("The names you entered are:", data)
+print("The names you entered are:", data)
 
-
+scores = {}
+for i, name in enumerate(data):
+    print(f"\n{name}, enter scores for each user (including yourself):")
+    total_score = 0
+    for j, other_user in enumerate(data):
+        while True:
+            score = input(f"Score for {other_user}: ")
+            if score.isdigit() and 0 <= int(score) <= 10:
+                break
+            else:
+                print("Invalid score, please enter a number between 0 and 10.")
