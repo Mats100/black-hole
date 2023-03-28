f626ab827ea5bcf7e4eee0ae8b4ad2aa70e5eef0
Mats100 <muneeb75@icloud.com>
2023-03-28 16:26:17 +0500

    new directory made

--- input_ops/v1/user_choices.py
+++ input_ops/v1/user_choices.py
@@ -7,12 +7,18 @@ print("The names you entered are:", data)
 
 scores = {}
 for i, name in enumerate(data):
-    print(f"\n{name}, enter scores for each user (including yourself):")
-    total_score = 0
+    print(f"\n{name}, Enter scores for each user (including yourself):")
     for j, other_user in enumerate(data):
         while True:
             score = input(f"Score for {other_user}: ")
             if score.isdigit() and 0 <= int(score) <= 10:
                 break
             else:
-                print("Invalid score, please enter a number between 0 and 10.")
+                print("Invalid score, Please enter a number between 0 and 10.")
+        scores[(i, j)] = int(score)
+
+print("\nTotal Scores:")
+for i, name in enumerate(data):
+    user_score = sum([scores[(j, i)] for j in range(choices) if j != i])
+    user_score += scores[(i, i)]
+    print(f"{name} got a total score : {user_score}")
